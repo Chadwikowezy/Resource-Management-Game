ac982f1a24355773eef2368fe04f5a5a1762eb5d
Chad McLeavy <chad_mcleavy@hotmail.com>
2017-04-29 20:01:38 -0700

    created resource handler to house all resources

---
+++ Resource Managment/Assets/Scripts/ResourceHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceHandler : MonoBehaviour
+{
+    [SerializeField]
+    private int _gold;
+    
+    //Properties
+    public int Gold
+    {
+        get { return _gold; }
+        set
+        {
+            _gold = value;
+            _gold = Mathf.Clamp(_gold, 0, _gold);
+        }
+    }
+}
---
+++ Resource Managment/Assets/Scripts/TestResourceHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestResourceHandler : MonoBehaviour
+{
+    public ResourceHandler resourceHandler;
+	
+	void Start ()
+    {
+        resourceHandler = FindObjectOfType<ResourceHandler>();
+	}
+	void Update ()
+    {
+	    	
+	}
+}
